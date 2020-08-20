5f4f69c5021ba07e9ce6153e8b2686c6c7c6717b
neerajEdu <35687019+neerajEdu@users.noreply.github.com>
2020-08-20 22:12:23 +0530

    Update SQL_PROB_INIT.sql

--- SQL_PROB_INIT.sql
+++ SQL_PROB_INIT.sql
@@ -77,3 +77,17 @@ WHERE Occupation = 'Actor'
 
 SELECT ROUND(SQRT(POWER(MAX(LAT_N) - MIN(LAT_N), 2) + POWER(MAX(LONG_W) - MIN(LONG_W), 2)), 4)
 FROM STATION;
+                  
+    
+-- A median is defined as a number separating the higher half of a data set from the lower half.
+-- Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.                  
+                  
+SELECT CAST(AVG(1.0 * LAT_N) AS DECIMAL (20,4))
+FROM
+(
+SELECT LAT_N, 
+ROW_NUMBER() OVER (ORDER BY LAT_N, id) ra,
+ROW_NUMBER() OVER (ORDER BY LAT_N DESC, id DESC) rd
+FROM STATION
+) AS x
+WHERE ra BETWEEN rd - 1 AND rd + 1;                
